e2d9fcd1fad288163b3f7ba9b3f06fa2296280f4
BlackViking98 <matvey.penkov@gmail.com>
2024-05-18 21:12:38 +0300

    My comment

---
+++ RMPOKS PZ7/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMPOKS_PZ7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] logins = new string[] {"student","vasya","director","uchitel" };
+            //пароль каждого аккаунта по номеру в массиве соотвествует номеру в массиве логинов,
+            //т.е. логин student пароль 12345
+            string[] passwords = new string[] {"12345","pupkin","wowdirector","gooduchitel"};
+            int counter = 3;
+                     
+            while (counter>0) {
+                Console.WriteLine("Enter login: ");
+                string login = Console.ReadLine();
+                        for (int i = 0; i < logins.Length; i++)
+                        {
+                            if (logins[i]==login) 
+                            {
+                                while (counter>0) {
+                                    Console.WriteLine("Enter password: ");
+                                    string pswdEntered = Console.ReadLine();
+
+                                    if (passwords[i] == pswdEntered)
+                                    {
+                                        Console.WriteLine("Enter the system");
+                                        counter = 0;
+                                        i =logins.Length;
+                                        break;
+                                    }
+                                    else
+                                    {
+                                        counter--;
+                                        Console.WriteLine("Incorrect password, please try again. Attempts remaining:" + counter);
+                                    }
+                                }
+                            } 
+                        }
+                if (counter>0) {
+                    counter--;
+                    Console.WriteLine("Incorrect login, please try again. Attempts remaining:" + counter);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
