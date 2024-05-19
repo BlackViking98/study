0bd5779ba215f8ee27948de9973baef318e28d52
Matvey Penkov <100151370+BlackViking98@users.noreply.github.com>
2024-05-19 14:33:22 +0300

    Add files via upload
    Расчет идеального веса по Лоренцу Вер.1

---
+++ RMPOKS PZ10 LorenzWeightCalculator/RMPOKS PZ10 LorenzWeightCalculator/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace RMPOKS_PZ10_LorenzWeightCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxHeight = new System.Windows.Forms.TextBox();
+            this.textBoxAge = new System.Windows.Forms.TextBox();
+            this.rbmale = new System.Windows.Forms.RadioButton();
+            this.rbfemale = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ideal_weight = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxHeight
+            // 
+            this.textBoxHeight.Location = new System.Drawing.Point(128, 170);
+            this.textBoxHeight.Name = "textBoxHeight";
+            this.textBoxHeight.Size = new System.Drawing.Size(100, 20);
+            this.textBoxHeight.TabIndex = 0;
+            this.textBoxHeight.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBoxAge
+            // 
+            this.textBoxAge.Location = new System.Drawing.Point(128, 206);
+            this.textBoxAge.Name = "textBoxAge";
+            this.textBoxAge.Size = new System.Drawing.Size(100, 20);
+            this.textBoxAge.TabIndex = 1;
+            this.textBoxAge.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // rbmale
+            // 
+            this.rbmale.AutoSize = true;
+            this.rbmale.Location = new System.Drawing.Point(6, 19);
+            this.rbmale.Name = "rbmale";
+            this.rbmale.Size = new System.Drawing.Size(71, 17);
+            this.rbmale.TabIndex = 2;
+            this.rbmale.TabStop = true;
+            this.rbmale.Text = "Мужской";
+            this.rbmale.UseVisualStyleBackColor = true;
+            this.rbmale.CheckedChanged += new System.EventHandler(this.rbmale_CheckedChanged);
+            // 
+            // rbfemale
+            // 
+            this.rbfemale.AutoSize = true;
+            this.rbfemale.Location = new System.Drawing.Point(6, 42);
+            this.rbfemale.Name = "rbfemale";
+            this.rbfemale.Size = new System.Drawing.Size(72, 17);
+            this.rbfemale.TabIndex = 3;
+            this.rbfemale.TabStop = true;
+            this.rbfemale.Text = "Женский";
+            this.rbfemale.UseVisualStyleBackColor = true;
+            this.rbfemale.CheckedChanged += new System.EventHandler(this.rbfemale_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbmale);
+            this.groupBox1.Controls.Add(this.rbfemale);
+            this.groupBox1.Location = new System.Drawing.Point(41, 84);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(112, 66);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Пол";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(153, 324);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 25);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Рассчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 173);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Ваш рост";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 209);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Ваш возраст";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(37, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(268, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Расчет веса по формуле Лоренца";
+            this.label3.UseMnemonic = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(38, 284);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(256, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Ваш идеальный вес по Лоренцу:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // ideal_weight
+            // 
+            this.ideal_weight.AutoSize = true;
+            this.ideal_weight.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ideal_weight.Location = new System.Drawing.Point(306, 284);
+            this.ideal_weight.Name = "ideal_weight";
+            this.ideal_weight.Size = new System.Drawing.Size(18, 20);
+            this.ideal_weight.TabIndex = 10;
+            this.ideal_weight.Text = "0";
+            this.ideal_weight.Click += new System.EventHandler(this.weight_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.ideal_weight);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBoxAge);
+            this.Controls.Add(this.textBoxHeight);
+            this.Name = "Form1";
+            this.Text = "РМПОКС ПЗ 10 Задание 2 (Пеньков М.М.)";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxHeight;
+        private System.Windows.Forms.TextBox textBoxAge;
+        private System.Windows.Forms.RadioButton rbmale;
+        private System.Windows.Forms.RadioButton rbfemale;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label ideal_weight;
+    }
+}
+
