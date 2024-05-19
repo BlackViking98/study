e9187888db95609bca7594c2a01f629cbba59d2a
Matvey Penkov <100151370+BlackViking98@users.noreply.github.com>
2024-05-19 13:51:50 +0300

    Add files via upload
    Калькулятор расчета идеального веса по ВРК

---
+++ Calculator VRK/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,323 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rbmale = new System.Windows.Forms.RadioButton();
+            this.rbfemale = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.radioButton8 = new System.Windows.Forms.RadioButton();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ideal_weight = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rbmale
+            // 
+            this.rbmale.AutoSize = true;
+            this.rbmale.Location = new System.Drawing.Point(6, 19);
+            this.rbmale.Name = "rbmale";
+            this.rbmale.Size = new System.Drawing.Size(71, 17);
+            this.rbmale.TabIndex = 0;
+            this.rbmale.TabStop = true;
+            this.rbmale.Text = "Мужской";
+            this.rbmale.UseVisualStyleBackColor = true;
+            this.rbmale.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // rbfemale
+            // 
+            this.rbfemale.AutoSize = true;
+            this.rbfemale.Location = new System.Drawing.Point(6, 42);
+            this.rbfemale.Name = "rbfemale";
+            this.rbfemale.Size = new System.Drawing.Size(72, 17);
+            this.rbfemale.TabIndex = 1;
+            this.rbfemale.TabStop = true;
+            this.rbfemale.Text = "Женский";
+            this.rbfemale.UseVisualStyleBackColor = true;
+            this.rbfemale.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbfemale);
+            this.groupBox1.Controls.Add(this.rbmale);
+            this.groupBox1.Location = new System.Drawing.Point(39, 64);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(113, 72);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Пол";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(6, 19);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(96, 17);
+            this.radioButton3.TabIndex = 3;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Астенический";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton5);
+            this.groupBox2.Controls.Add(this.radioButton4);
+            this.groupBox2.Controls.Add(this.radioButton3);
+            this.groupBox2.Location = new System.Drawing.Point(39, 142);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 100);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Телосложение";
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(6, 65);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(119, 17);
+            this.radioButton5.TabIndex = 5;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "Гиперстенический";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.CheckedChanged += new System.EventHandler(this.radioButton5_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(6, 42);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(123, 17);
+            this.radioButton4.TabIndex = 4;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Нормостенический";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.radioButton8);
+            this.groupBox3.Controls.Add(this.radioButton7);
+            this.groupBox3.Controls.Add(this.radioButton6);
+            this.groupBox3.Location = new System.Drawing.Point(39, 248);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(200, 100);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Возраст";
+            // 
+            // radioButton8
+            // 
+            this.radioButton8.AutoSize = true;
+            this.radioButton8.Location = new System.Drawing.Point(6, 65);
+            this.radioButton8.Name = "radioButton8";
+            this.radioButton8.Size = new System.Drawing.Size(52, 17);
+            this.radioButton8.TabIndex = 6;
+            this.radioButton8.TabStop = true;
+            this.radioButton8.Text = "26-40";
+            this.radioButton8.UseVisualStyleBackColor = true;
+            this.radioButton8.CheckedChanged += new System.EventHandler(this.radioButton8_CheckedChanged);
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.Location = new System.Drawing.Point(6, 42);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(52, 17);
+            this.radioButton7.TabIndex = 1;
+            this.radioButton7.TabStop = true;
+            this.radioButton7.Text = "19-25";
+            this.radioButton7.UseVisualStyleBackColor = true;
+            this.radioButton7.CheckedChanged += new System.EventHandler(this.radioButton7_CheckedChanged);
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(6, 19);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(52, 17);
+            this.radioButton6.TabIndex = 0;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "15-18";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            this.radioButton6.CheckedChanged += new System.EventHandler(this.radioButton6_CheckedChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApp1.Properties.Resources.c8198e24d7871ee2bbf4a3f9bb7fe6b8;
+            this.pictureBox1.Location = new System.Drawing.Point(320, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(458, 321);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(39, 374);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(216, 31);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Идеальный вес:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // ideal_weight
+            // 
+            this.ideal_weight.AutoSize = true;
+            this.ideal_weight.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ideal_weight.Location = new System.Drawing.Point(261, 374);
+            this.ideal_weight.Name = "ideal_weight";
+            this.ideal_weight.Size = new System.Drawing.Size(29, 31);
+            this.ideal_weight.TabIndex = 8;
+            this.ideal_weight.Text = "0";
+            this.ideal_weight.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(122, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Ваш рост в см";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(102, 408);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Рассчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(588, 428);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(190, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Разработал ст.гр.128 Пеньков М.М.";
+            this.label2.Click += new System.EventHandler(this.label2_Click_1);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(401, 351);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(307, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Нажмите на картинку, чтобы узнать о типах телосложения";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RosyBrown;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.ideal_weight);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Расчет идеального веса по ВРК";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rbmale;
+        private System.Windows.Forms.RadioButton rbfemale;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RadioButton radioButton8;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label ideal_weight;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Calculator VRK/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        int koef = 0;
+        int height = 0;
+        double result = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calculate() {
+            if (rbfemale.Checked && radioButton3.Checked && radioButton6.Checked) {
+                koef = 315;    
+            } else if (rbfemale.Checked && radioButton3.Checked && radioButton7.Checked) {
+                koef = 325;
+            } else if (rbfemale.Checked && radioButton3.Checked && radioButton8.Checked) {
+                koef = 335;
+            } else if (rbfemale.Checked && radioButton4.Checked && radioButton6.Checked) {
+                koef = 325;
+            } else if (rbfemale.Checked && radioButton4.Checked && radioButton7.Checked) {
+                koef = 345;
+            } else if (rbfemale.Checked && radioButton4.Checked && radioButton8.Checked) {
+                koef = 360;
+            } else if (rbfemale.Checked && radioButton5.Checked && radioButton6.Checked) {
+                koef = 355;
+            } else if (rbfemale.Checked && radioButton5.Checked && radioButton7.Checked) {
+                koef = 370;
+            } else if (rbfemale.Checked && radioButton5.Checked && radioButton8.Checked) {
+                koef = 380;
+            } else {
+                koef = 0;
+                MessageBox.Show("Рассчет невозможен");
+            }
+
+            if (!Int32.TryParse(textBox1.Text, out height))
+            {
+                MessageBox.Show($"Введенное значение в поле Рост {textBox1.Text} не распознано, как целочисленное. Повторите ввод");
+            }
+            else
+            {
+                Int32.TryParse(textBox1.Text, out height);
+                if (height < 100)
+                {
+                    MessageBox.Show("Рост должен быть больше 1 метра!");
+                }
+                else
+                {
+                    result = (height * koef) / 1000;
+                    ideal_weight.Text = Convert.ToString(result);
+                }
+            }
+
+
+        }
+
+            private void radioButton1_CheckedChanged(object sender, EventArgs e) //rbmale
+        {
+            
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e) //rbfemale
+        {
+
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e) //asten
+        {
+
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e) //normosten
+        {
+
+        }
+
+        private void radioButton5_CheckedChanged(object sender, EventArgs e) //hypersten
+        {
+
+        }
+
+        private void radioButton6_CheckedChanged(object sender, EventArgs e) //15-18
+        {
+
+        }
+
+        private void radioButton7_CheckedChanged(object sender, EventArgs e) //19-25
+        {
+
+        }
+
+        private void radioButton8_CheckedChanged(object sender, EventArgs e) //26-40
+        {
+
+        }
+
+
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Нормостенический тип — это пропорционально сложенный человек с хорошо развитой мускулатурой, с широкими плечами, выпуклой грудью, небольшим упругим животом и средней длиной конечностей с хорошо развитыми на них мышцами. " +
+                "Люди нормостенического типа энергичны, уверены в своих силах. У них отмечается склонность к заболеваниям верхних дыхательных путей, двигательного аппарата, невралгиям, атеросклерозу коронарных сосудов. " +
+                "\n\nАстенический тип в сравнении с нормостеническим характеризуется преимущественным развитием тела в длину, стройностью и легкостью его строения. " +
+                "Характерны высокий рост, узкая, плоская грудная клетка, покатые плечи, длинная шея, тонкие и длинные конечности, продолговатое лицо, слабое развитие мускулатуры, бледная, тонкая кожа, внутренние органы (сердце, легкие, кишечник и др.) относительно малых размеров. Люди с астеническим типом отличаются повышенной возбудимостью нервной системы, склонностью к птозу (опущению) внутренних органов, неврозам, гипотензии, к туберкулезу, язвенной болезни." +
+                "\n\nГиперстенический тип характеризуется большой развитостью в ширину. У таких людей относительно короткое, массивное, упитанное туловище и короткие конечности. Рост средний и ниже среднего. " +
+                "Голова круглая, череп большой, шея короткая, плечи широкие, крутые, грудная клетка короткая, широкая, живот большой, выпячен. Размеры сердца сравнительно большие, величина легких относительно мала.\r\nИсточник: https://meduniver.com/Medical/pulmonologia/428.html MedUniver");
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            calculate();
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
